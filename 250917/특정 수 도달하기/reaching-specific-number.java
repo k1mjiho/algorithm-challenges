ecd96308eea5e85f16a7dda8e12586c078577879
jiho <47881023+k1mjiho@users.noreply.github.com>
2025-09-17 23:36:15 +0900

    Title: 특정 수 도달하기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250917/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String row = br.readLine();
+
+        String[] arr = row.split(" ");
+        int sum = 0;
+        double evg = 0.0;
+        int cnt = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            int tmp = Integer.parseInt(arr[i]);
+            if(tmp >= 250) { 
+                cnt = i;
+                break;
+                }
+            sum += tmp;
+        }
+
+        evg = (double) sum / cnt;
+        System.out.print(sum + " " + evg);
+    }
+}
